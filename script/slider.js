cbd7c3b31da37606ff4de9095eaa7e76b18f7f28
wildannugraha99 <mwnwildan99@gmail.com>
2021-09-09 11:40:23 +0700

    update minor

--- script/slider.js
+++ script/slider.js
@@ -17,8 +17,8 @@ const swiperSertif = new Swiper(".swiper-sertif", {
 
 
 const swiperProject = new Swiper(".swiper-project", {
-  slidesPerView: 1.5,
-  spaceBetween: 30,
+  slidesPerView: 2,
+  spaceBetween: 15,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
